d783d5bfdc1281e8655a23e2b3d9f0b749d6f43a
Nicklas <nicklas.oest@gmail.com>
2017-04-05 15:33:09 +0200

    update Dbhelper class

--- app/src/main/java/se/nios/sensorapp/DBHelper.java
+++ app/src/main/java/se/nios/sensorapp/DBHelper.java
@@ -1,8 +1,13 @@
 package se.nios.sensorapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 /**
  * Created by Nicklas on 2017-04-05.
@@ -29,12 +34,72 @@ public class DBHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(
                 "create table sensors " +
-                        "(id integer primary key, name text,phone text,email text, street text,place text)"
+                        "(id integer primary key, seqno text,payload text,temperature text, humidity text,light text, motionCounter text, battery text)"
         );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS sensors");
+        onCreate(db);
+    }
+    public boolean insertSensorData(String seqno,String payload,String temperature, String humidity, String light, String motionCounter,String battery){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("seqno", seqno);
+        contentValues.put("payload", payload);
+        contentValues.put("temperature", temperature);
+        contentValues.put("humidity", humidity);
+        contentValues.put("light", light);
+        contentValues.put("motionCounter",motionCounter);
+        contentValues.put("battery",battery);
+        db.insert("sensors", null, contentValues);
+        return true;
+    }
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from sensors where id="+id+"", null );
+        return res;
+    }
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, SENSORS_TABLE_NAME);
+        return numRows;
+    }
 
+    public boolean updateSensorValue (Integer id,String seqno,String payload,String temperature, String humidity, String light, String motionCounter,String battery) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("seqno", seqno);
+        contentValues.put("payload", payload);
+        contentValues.put("temperature", temperature);
+        contentValues.put("humidity", humidity);
+        contentValues.put("light", light);
+        contentValues.put("motionCounter",motionCounter);
+        contentValues.put("battery",battery);
+        db.update("sensors", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+        return true;
+    }
+
+    public Integer deleteSensorValue (Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("sensors",
+                "id = ? ",
+                new String[] { Integer.toString(id) });
+    }
+
+    public ArrayList<String> getAllSensors() {
+        ArrayList<String> array_list = new ArrayList<String>();
+
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from sensors", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            array_list.add(res.getString(res.getColumnIndex(SENSORS_TABLE_NAME)));
+            res.moveToNext();
+        }
+        return array_list;
     }
 }
